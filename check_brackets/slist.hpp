b16df37a056f95ebcb3b614cd6ce73f7d7297b9d
aneri00 <i82andel@uco.es>
2021-03-17 21:47:03 +0100

    final

---
+++ check_brackets/slist.hpp
@@ -0,0 +1,756 @@
+#ifndef __ED_SList_HPP__
+#define __ED_SList_HPP__
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <cassert>
+
+/** @brief a single link node.*/
+
+template <class T>
+class SNode
+{
+public:
+
+    /** @brief Define a shared reference to a SNode.
+     * Manages the storage of a pointer, providing a limited garbage-collection
+     * facility, possibly sharing that management with other objects.
+     * @see http://www.cplusplus.com/reference/memory/shared_ptr/
+     */
+    typedef std::shared_ptr< SNode<T> > Ref;
+
+    /** @name Life cicle.*/
+    /** @{*/
+
+    /** @brief Create a node.
+     * @post !has_next()
+     */
+    SNode (T const& it)
+    {
+        //TODO
+        nodeData = it;
+        nextNode = nullptr;
+        assert(!has_next());
+    }
+
+    /** @brief Create an empty Stack.*/
+    SNode (T const& it, SNode<T>::Ref& next)
+    {
+        //TODO
+        nodeData = it;
+        nextNode = next;
+    }
+
+    /**
+     * @brief Create a SNode using dynamic memory.
+     * @param it the value save in the node.
+     * @param next link to the next node.
+     * @return a shared referente to the new node.
+     */
+    static typename SNode<T>::Ref create(T const& it, SNode<T>::Ref next=nullptr)
+    {
+        return std::make_shared<SNode<T>> (it, next);
+    }
+
+    /** @brief Destroy a SNode.**/
+    ~SNode()
+    {
+        //TODO: think if it is needed.
+    }
+    /** @}*/
+
+    /** @name Observers.*/
+    /** @{*/
+
+    /** @brief Get the data item.*/
+    const T& item() const
+    {
+        //TODO
+        return nodeData;
+    }
+
+    /**
+     * @brief Has it a next node?.
+     * @return true if it has a next node.
+    */
+    bool has_next() const
+    {
+
+        //TODO
+
+        if (nextNode != nullptr)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    /** @brief Get the link to next element.*/
+    SNode<T>::Ref next() const
+    {
+        //TODO
+        return nextNode;
+    }
+
+    /** @}*/
+
+    /** @name Modifiers.*/
+    /** @{*/
+
+    /** @brief Set the data item.*/
+    void set_item(const T& new_it)
+    {
+        nodeData = new_it;
+    }
+
+    /** @brief Set the link to the next node.*/
+    void set_next(SNode<T>::Ref next)
+    {
+        nextNode = next;
+    }
+
+protected:
+
+    //TODO
+    T nodeData;
+    SNode<T>::Ref nextNode;
+
+};
+
+/**
+ * @brief ADT SList.
+ * Models a Single linked list[T].
+ */
+template<class T>
+class SList
+{
+  public:
+
+    /** @brief Define a shared reference to a SNode.
+     * Manages the storage of a pointer, providing a limited garbage-collection
+     * facility, possibly sharing that management with other objects.
+     * @see http://www.cplusplus.com/reference/memory/shared_ptr/
+     */
+    typedef std::shared_ptr< SList<T> > Ref;
+
+  /** @name Life cicle.*/
+  /** @{*/
+
+  /** @brief Create an empty Stack.
+   * @post is_empty()
+   */
+  SList ()
+  {
+      //TODO
+      head = nullptr;
+<<<<<<< HEAD
+      current_ =  head;
+=======
+      current =  head;
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      assert(is_empty());
+  }
+
+  /** @brief Destroy a Stack.**/
+  ~SList()
+  {}
+
+  /**
+   * @brief Create a SList using dynamic memory.
+   * @return a shared referente to the new slist.
+   */
+  static typename SList<T>::Ref create()
+  {
+      return std::make_shared<SList<T>> ();
+  }
+
+  /**
+   * @brief Create a SList unfoldig from an input stream.
+   *
+   * The input format will be "[]" for the empty list
+   * or "[" item1 item2 ... item_n "]" where item1 is the head.
+   *
+   * @param in is the input stream.
+   * @warning if the input format is not correct a std::runtime_error with
+   * what message "Wrong input format." will be reaised.
+   * @return A shared referente to the new slist.
+   */
+  static typename SList<T>::Ref create(std::istream& in) noexcept(false)
+  {      
+      auto list = SList<T>::create();
+
+      //TODO
+      //Hint: use std::istringstream to convert a token into generic T type.
+<<<<<<< HEAD
+
+      std::string token;
+      std::getline(in, token, '\n');
+
+      std::string aux = token.substr(token.find(' ')+1);
+
+      if (aux[0] != '[' or aux[aux.size()-1] != ']'){
+         
+         throw std::runtime_error("Worng input format.");
+
+      }
+      if (aux == "[]") { 
+
+        return list;
+
+      }
+
+      aux = aux.substr(2, aux.size()-4);
+
+      int espacios = 0;
+
+      for (auto aux2 : aux) if (aux2 == ' ') espacios++;
+      espacios++;
+
+      std::istringstream translater(aux);
+
+      for (int i = 0; i < espacios; ++i)
+      {
+        int cant;
+        translater >> cant;
+        list->insert(cant);
+        if (i != 0)
+        {
+
+          list->goto_next();
+
+        }
+        
+      }
+
+=======
+      
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      return list;
+
+
+
+
+  }
+
+  /** @}*/
+
+  /** @name Observers*/
+
+  /** @{*/
+
+  /** @brief is the list empty?.*/
+  bool is_empty () const
+  {
+
+      //TODO
+<<<<<<< HEAD
+      if (head == nullptr)
+=======
+      if (head ==  nullptr)
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      {
+          return true;
+      }
+      
+      return false;
+  }
+
+  /**
+   * @brief Get the head's item of the list.
+   * @return the item at the head.
+   */
+  const T& front() const
+  {
+      assert(!is_empty());
+      //TODO
+      return head->item();
+  }
+
+  /** @brief get the current item.
+   * @pre not is_empty()
+   */
+  const T& current() const
+  {
+      assert(! is_empty());
+      //TODO
+<<<<<<< HEAD
+      return current_->item();
+=======
+      return current->item();
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+  }
+
+  /**
+   * @brief Is there a next item?
+   * @return true if there is.
+   * @pre !is_empty()
+   */
+  bool has_next() const
+  {
+      assert(!is_empty());
+      //TODO
+<<<<<<< HEAD
+      if (current_->next() != nullptr)
+=======
+      if (current->next() != NULL)
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      {
+          return true;
+      }
+      
+      return false;
+  }
+
+  /**
+   * @brief Get the next item.
+   * @return the next item data.
+   * @pre has_next()
+   */
+  T const& next() const
+  {
+      //TODO
+      assert(has_next());
+<<<<<<< HEAD
+      return current_->next()->item();
+=======
+      return auxNode->next()->item();
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+  }
+
+
+  /**
+   * @brief Has it the item data?
+   * @param[in] it is the item to find.
+   * @return true if the item is into the list.
+   */
+  bool has(T const& it) const
+  {
+      //TODO
+      typename SNode<T>::Ref auxNode = head;
+<<<<<<< HEAD
+      while (auxNode != nullptr)
+      {
+          if(auxNode->item() == it){
+
+              return true;
+
+          }
+
+=======
+      while (auxNode->has_next())
+      {
+          if(auxNode->item() == it){
+
+              return true;
+
+          }
+
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+          auxNode = auxNode->next();
+      }
+
+      return false;
+  }
+
+  /**
+   * @brief Fold to an output stream.
+   *
+   * The format will be "[]" for the empty list or  '[' item1 item2 item3 ... item_n ']'
+   * where item1 is the head.
+   *
+   * @param out is the output stream.
+   * @return the output stream.
+   */
+
+  void fold(std::ostream& out) const
+  {
+      //TODO
+      typename SNode<T>::Ref auxNode = head;
+      if (is_empty())
+      {
+          out<<"[]";
+      }else{
+
+<<<<<<< HEAD
+          out<<"[";
+
+          while (auxNode != nullptr)
+          {
+
+              out<<" "<<auxNode->item();
+              auxNode = auxNode->next();
+              
+          }
+
+=======
+          out<<"[ ";
+          while (auxNode->hasNext())
+          {
+              out<<" "<<auxNode->item();
+              auxNode = auxNode->next();
+
+          }
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+          out<<" ]";
+      }
+      
+  }
+
+  /**@}*/
+
+  /** @name Modifiers*/
+
+  /** @{*/
+
+  /**
+   * @brief Set a new value for current.
+   * @param new_v is the new value.
+   * @pre !is_empty()
+   * @post item()==new_v
+   */
+  void set_current(T const& new_v)
+  {
+      assert(!is_empty());
+      
+      //TODO
+<<<<<<< HEAD
+      current_->set_item(new_v);
+=======
+      current->set_item(new_v);
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+
+      assert(current()==new_v);
+  }
+
+
+  /**
+   * @brief insert an item as the new list's head.
+   * @param new_it is the item to insert.
+   * @post head()==new_it
+   */
+  void push_front(T const& new_it)
+  {
+      //TODO
+      if (is_empty())
+      {
+<<<<<<< HEAD
+
+          head = SNode<T>::create(new_it, head);
+          current_ = head;
+
+      }else{
+
+      	  auto newHead = SNode<T>::create(new_it, head);
+      	  head = newHead;
+      	  current_ = head;
+
+      }
+
+
+
+
+=======
+
+          head = SNode<T>::create(new_it, nullptr);
+          current = head;
+
+      }
+      
+      auto newHead = SNode<T>::create(new_it, head);
+      head = newHead;
+
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      assert(front()==new_it);
+  }
+
+
+  /**
+   * @brief insert a new item after current.
+   * @param new_it is the item to insert.
+   * @post old.is_empty() implies item()==head()==new_it
+   * @post !old.is_empty() implies item()==old.item() && has_next() && next()==new_it
+   */
+  void insert(T const& new_it)
+  {
+#ifndef NDEBUG
+      auto old_is_empty = is_empty();
+      T old_item;
+      if (!old_is_empty)
+          old_item = current ();
+#endif
+
+      //TODO
+      if (is_empty())
+      {
+<<<<<<< HEAD
+          push_front(new_it);
+
+      }else{
+
+          auto newNode = SNode<T>::create(new_it, current_->next());
+          current_->set_next(newNode);
+          //current_ = newNode;
+=======
+          push_front(new_it, head);
+          current = head;
+
+      }else{
+
+          auto newNode = SNode<T>::create(new_it, current->next());
+          current->set_next(newNode);
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+
+      }
+
+      assert(!old_is_empty || (front()==new_it && current()==new_it));
+      assert(old_is_empty || (old_item == current() && has_next() && next()==new_it));
+  }
+
+  /**
+   * @brief Remove the head.
+   * @prec !is_empty()
+   * @post is_empty() || head() == "next of old.head()".
+   */
+  void pop_front()
+  {
+      assert(!is_empty());
+      //TODO
+<<<<<<< HEAD
+
+      //T old_head;
+
+
+      if(head->has_next()){
+      	//old_head = head->item();
+        head = head->next();
+        current_ = head;
+        
+      }
+
+      else{
+          head = nullptr;
+          current_ = head;
+      }
+
+
+
+     //assert(is_empty() || (front()==old_head));
+=======
+    
+      auto old_head = head->next()->item();
+      head = head->next();
+      current = head;
+      assert(is_empty() || (front()==old_head));
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+  }
+
+
+  /**
+   * @brief Remove current item.
+   * @pre !is_empty()
+   * @post old.has_next() implies current()==old.next()
+   * @post !old.has_next() implies is_empty() || current()=="old previous item."
+   */
+  void remove()
+  {
+      assert(!is_empty());
+#ifndef NDEBUG
+      bool old_has_next = has_next();
+      T old_next;
+      if (has_next())
+          old_next=next();
+#endif
+
+      //TODO:
+      //Study three cases: remove from head, remove from last and
+      //remove from middle.
+<<<<<<< HEAD
+      if (current_ == head)
+      {
+          pop_front();
+
+      }else if(!current_->has_next()){
+
+          auto auxNode = head;
+          while (auxNode->next() != current_)
+          {
+              auxNode = auxNode->next();
+          }
+          
+          auxNode->set_next(nullptr);
+          current_ = auxNode;
+=======
+      if (current == head)
+      {
+          pop_front();
+
+      }else if(!current->has_next()){
+
+          auto auxNode = head;
+          while (auxNode != current)
+          {
+              aux = aux->next();
+          }
+          
+          auxNode->set_next(nullptr);
+          current = auxNode;
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+
+      }else{
+
+          auto auxNode = head;
+<<<<<<< HEAD
+          while (auxNode->next() != current_)
+          {
+              auxNode = auxNode->next();
+          }
+          
+          auxNode->set_next(current_->next());
+          current_ = current_->next();
+=======
+          while (auxNode != current)
+          {
+              aux = aux->next();
+          }
+          
+          auxNode->set_next(current->next());
+          current = current->next();
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+
+      }
+      
+
+      assert(!old_has_next || current()==old_next);
+  }
+
+  /**
+   * @brief Move the cursor to the next list's item.
+   * @pre has_next()
+   * @post old.next()==current()
+   */
+  void goto_next()
+  {
+      assert(has_next());      
+#ifndef NDEBUG
+      auto old_next = next();
+#endif
+
+      //TODO
+<<<<<<< HEAD
+
+      current_= current_->next();
+=======
+      current= current->nex();
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      assert(current()==old_next);
+  }
+
+  /**
+   * @brief Move the cursor to the list's head.
+   * @pre !is_empty()
+   * @post current()==front()
+   */
+  void goto_first()
+  {
+      assert(!is_empty());
+<<<<<<< HEAD
+      current_= head;
+=======
+      current= head;
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+      assert(current()==front());
+  }
+
+  /**
+   * @brief Move the cursor to the first occurrence of a value from the head of the list.
+   * If the item is not found, the cursor will be at the end of the list.
+   * @param it is the value to be found.
+   * @return true if it is found.
+   * @pre !is_empty()
+   * @post !ret_val || item()==it
+   * @post ret_value || !has_next()
+   */
+  bool find(T const& it)
+  {
+      assert(!is_empty());
+      bool found = false;
+      //TODO
+      current_ = head;
+
+      while( current_ != nullptr ){
+
+          if(current_->item() == it){
+              
+              found = true;
+              return found;
+          }
+
+            current_ = current_->next();
+
+          
+
+      }
+
+      assert(!found || current()==it);
+      assert(found || !has_next());
+      return found;
+  }
+
+  /**
+   * @brief Move the cursor to the next occurrence of a value from current.
+   * If the item is not found, the cursor will be at the end of the list.
+   * @param it is the value to be found.
+   * @return true if it is found.
+   * @pre has_next()
+   * @post !ret_val || item()==it
+   * @post ret_value || !has_next()
+   */
+  bool find_next(T const& it)
+  {
+      assert(!is_empty());
+      bool found = false;
+      //TODO
+
+      current_ = current_->next();
+
+      while( current_ != nullptr ){
+
+          if(current_->item() == it){
+              
+              found = true;
+              return found;
+          }
+
+            current_ = current_->next();
+
+      }
+
+      return found;
+      assert(!found || current()==it);
+      assert(found || !has_next());
+      
+  
+}
+
+  /** @}*/
+
+
+protected:
+
+  //TODO
+  typename SNode<T>::Ref head;
+<<<<<<< HEAD
+  typename SNode<T>::Ref current_;
+=======
+  typename SNode<T>::Ref current;
+
+>>>>>>> 74b6454e03b87cf93523210b917951a6a5bd85ac
+
+
+};
+
+#endif //ED_SList
